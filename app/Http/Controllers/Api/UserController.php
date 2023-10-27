9c159dc84afa17e5480b93e151905b786e4e777d
chrlncatalino <indemorlino@gmail.com>
2023-10-27 17:55:49 +0700

    Updated

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -3,10 +3,11 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\User;
+use App\Models\Flight;
 use Illuminate\Http\Request;
+use App\Http\Requests\UserRequest;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
-use App\Http\Requests\UserRequest;
 
 class UserController extends Controller
 {
@@ -25,6 +26,56 @@ class UserController extends Controller
     public function show(string $id)
     {
         return User::findOrFail($id);
+    }
+
+    /**
+     * Update the specified resource in storage
+     */
+    public function update(UserRequest $request, string $id)
+    {
+ 
+        $user = User::findOrFail($id);
+ 
+        $validated = $request->validated();
+
+        $user->name = $validated['name'];
+ 
+        $user->save(); 
+
+        return $user;
+    }
+
+     /**
+     * Update the email of the specified resource in storage
+     */
+    public function email(UserRequest $request, string $id)
+    {
+        
+        $user = User::findOrFail($id);
+ 
+        $validated = $request->validated();
+
+        $user->email = $validated['email'];
+ 
+        $user->save(); 
+
+        return $user;
+    }
+
+     /**
+     * Update the password specified resource in storage
+     */
+    public function password(UserRequest $request, string $id)
+    {
+        $user = User::findOrFail($id);
+ 
+        $validated = $request->validated();
+
+        $user->password = Hash::make($validated['password']);
+ 
+        $user->save(); 
+
+        return $user;
     }
 
     /**
